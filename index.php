80091613207ca7f440188cf07944ee09436ea302
= <=>
2026-09-10 10:43:42 +0700

    Buku-Tamu

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include_once('templates/header.php');
+?>
+
+<!-- Begin Page Content-->
+ <div class = "container-fluid">
+
+        <!-- Page Heading -->
+        <h1 class="h3 mb-4 text-grey-800">Dashboard Admin</h1>
+
+ </div>
+<!--/. container fluid -->
+
+<?php
+include_once('templates/footer.php')
+?>
