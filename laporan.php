32a5acf80ea10b44ae12928c80e58ac851a7007c
= <=>
2026-09-23 10:51:42 +0700

    Buku-Tamu

--- laporan.php
+++ laporan.php
@@ -1,4 +1,23 @@
 <?php
+include_once('function.php');
+include_once('templates/header.php');
+
+if (isset($_POST['tampilkan'])) {
+
+    $p_awal = $_POST['p_awal'];
+    $p_akhir = $_POST['p_akhir'];
+
+    $link = "export-laporan.php?cari=true&p_awal=$p_awal&p_akhir=$p_akhir";
+    // query sesuai dengan keyword
+    $buku_tamu = query("SELECT * FROM buku_tamu WHERE tanggal BETWEEN '$p_awal' AND '$p_akhir'");
+
+} else {
+    // query ambil semua data buku tamu
+    $buku_tamu = query("SELECT * FROM buku_tamu ORDER BY tanggal DESC");
+}
+?>
+
+<?php
 include_once('templates/header.php');
 ?>
 
@@ -51,11 +70,16 @@ include_once('templates/header.php');
     <!-- Tabel Histori Tamu ditaruh di bawah sini -->
 
 </div>
-
-<div class="card shadow mb-4">
+    <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <span class="text">Tabel Histori Tamu</span>
+        <a href="<?= isset($_POST['tampilkan']) ? $link : 'export-laporan.php'; ?>" target="_blank" class="btn btn-success btn-icon-split">
+            <span class="icon text-white-50">
+                <i class="fas fa-file-excel"></i>
+            </span>
+            <span class="text">Export Laporan</span>
+        </a>
     </div>
+</div>
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -73,30 +97,28 @@ include_once('templates/header.php');
                 </thead>
                 <tbody>
                     <?php
-    if (isset($_POST['tampilkan'])) {
-        $p_awal = $_POST['p_awal'];
-        $p_akhir = $_POST['p_akhir'];
-        // penomoran auto-increment
-        $no = 1;
-        // Query untuk memanggil semua data dari tabel buku_tamu
-        $buku_tamu = query("SELECT * FROM buku_tamu WHERE tanggal BETWEEN '$p_awal' AND '$p_akhir' ");
-        foreach ($buku_tamu as $tamu) : ?>
-            <tr>
-                <td><?= $no++; ?></td>
-                <td><?= $tamu['tanggal'] ?></td>
-                <td><?= $tamu['nama_tamu'] ?></td>
-                <td><?= $tamu['alamat'] ?></td>
-                <td><?= $tamu['no_hp'] ?></td>
-                <td><?= $tamu['bertemu'] ?></td>
-                <td><?= $tamu['kepentingan'] ?></td>
+                        // penomoran auto-increment
+                        $no = 1;
+
+                        foreach ($buku_tamu as $tamu) : ?>
+
+                <tr>
+                    <td><?= $no++; ?></td>
+                    <td><?= $tamu['tanggal'] ?></td>
+                    <td><?= $tamu['nama_tamu'] ?></td>
+                    <td><?= $tamu['alamat'] ?></td>
+                    <td><?= $tamu['no_hp'] ?></td>
+                    <td><?= $tamu['bertemu'] ?></td>
+                    <td><?= $tamu['kepentingan'] ?></td>
                 <td>
                     <a class="btn btn-success" href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>">Ubah</a>
-                    <a onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')" class="btn btn-danger" href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
+                    <a onclick="return confirm('Apakah yakin ingin menghapus data ini?')" class="btn btn-danger"
+                   href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
                 </td>
             </tr>
-                <?php endforeach;
-                    }
-                ?>
+
+    <?php endforeach;
+    ?>
                 </tbody>
             </table>
         </div>
